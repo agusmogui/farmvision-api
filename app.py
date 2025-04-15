cbdf4005b57df1d80d270ae381dba455d8c27675
juanagalamoran <tu-email@ejemplo.com>
2025-04-15 01:32:49 -0300

    Version 1.0.0.

--- app.py
+++ app.py
@@ -63,5 +63,8 @@ def cotizar():
         "ganancia_aplicada": f"{int(ganancia * 100)}%"
     })
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
